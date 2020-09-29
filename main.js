2cf821f5f6a08f90a9bd68f8e973ae006fc8a5a6
Tom Bousquet <tom.bousquet@alumni.unc.edu>
2020-09-29 17:54:39 -0400

    restructure

--- main.js
+++ main.js
@@ -2,33 +2,48 @@ const url = 'http://localhost:3000/notes'
 
 document.addEventListener('submit', function (event) {
     event.preventDefault()
+    createNote()
+})
+
+function renderNotesList () {
+    fetch(url)
+        .then(res => res.json())
+        .then(notes => {
+            let notesList = document.querySelector('#notesList')
+                for (let note of notes) {
+                    console.log(note)
+                addedNote = document.createElement('div')
+                    addedNote.classList.add('note-card')
+                const noteText = document.createElement('p')
+                    addedNote.appendChild(noteText)
+                    noteText.innerText = note.noteItem
+                notesList.appendChild(addedNote)
+                }
+            })
+        }
+
+function createNote() {
     const noteInput = document.querySelector('#noteInput').value
     console.log(noteInput)
 
-fetch(url, {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({
-        noteItem: noteInput,
-    })
-})
-    .then(res => res.json())
-    .then(data => {
-        const notesList = document.querySelector('#notesList')
-        const addedNote = document.createElement('li')
-        addedNote.innerText = data.noteItem
-        notesList.appendChild(addedNote)
-    })
-})
-    
-fetch(url)
-    .then(res => res.json())
-    .then(notes => {
-        let notesList = document.querySelector('#notesList')
-            for (let note of notes) {
-                console.log(note)
-            addedNote = document.createElement('li')
-            addedNote.innerText = notes.noteItem
-            notesList.appendChild(addedNote)
-            }
+    fetch(url, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            noteItem: noteInput,
         })
+    })
+        .then(res => res.json())
+        .then(data => {
+            const notesList = document.querySelector('#notesList')
+            const addedNote = document.createElement('div')
+                addedNote.classList.add('note-card')
+            const noteText = document.createElement('p')
+                addedNote.appendChild(noteText)
+                noteText.innerText = data.noteItem
+            notesList.appendChild(addedNote)
+        })
+    }
+    
+    renderNotesList()
+
